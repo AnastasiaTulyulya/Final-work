e01e2f10396530ec54c6f3b9ceb25e6148b94f8e
AnastasiaTulyulya <anastasiatulula@gmail.com>
2020-05-29 12:14:32 +0700

    Create app

--- App.js
+++ App.js
@@ -1,10 +1,38 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+
+function isPrime(num) {
+  let flag = true;
+  for (let i = 2; i < num; i++) {
+    if (num % i == 0) {
+      flag = false;
+      break;
+    }
+  }
+  return flag;
+}
 
 export default function App() {
+  const [numbers, setNumbers] = useState([]);
+
+  useEffect(() => {
+    let number = [];
+    for (let i = 1; i < 200; i++) {
+      if (isPrime(i)) {
+        number.push(<Text key={i} style={styles.number}>{i}</Text>);
+      }
+    }
+    setNumbers(number);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Простые числа от 0 до 200</Text>
+      <ScrollView contentContainerStyle={styles.contentContainerStyle} style={styles.scroll}>
+        {
+          numbers
+        }
+      </ScrollView>
     </View>
   );
 }
@@ -12,8 +40,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#7b426b',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 30,
+    paddingVertical: 30,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 12,
+    color: "#fbf5e6"
+  },
+  scroll: {
+    width: "100%",
+  },
+  contentContainerStyle: {
     alignItems: 'center',
     justifyContent: 'center',
   },
+  number: {
+    fontSize: 16,
+    color: "#fbf5e6"
+  }
 });
